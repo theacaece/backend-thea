7f5ea26ff1389874ac9b91c21c93a38b38b3cf28
Javier M <yjolter@gmail.com>
2020-01-16 01:56:07 -0300

    Version con llamada a reconocimiento funcionando, pendiente envíar y recibir datos binarios

--- src/main/java/edu/caece/app/service/impl/ReconocimientoServiceImpl.java
+++ src/main/java/edu/caece/app/service/impl/ReconocimientoServiceImpl.java
@@ -1,31 +1,20 @@
 package edu.caece.app.service.impl;
 
-import java.io.IOException;
-import java.net.URI;
-import java.net.http.HttpClient;
-import java.net.http.HttpRequest;
-import java.net.http.HttpResponse;
-import java.net.http.HttpResponse.BodyHandlers;
-
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
 
 import edu.caece.app.service.ReconocimientoService;
+import edu.caece.app.service.impl.dto.ReconocimientoResultsDto;
 
 @Service
 public class ReconocimientoServiceImpl implements ReconocimientoService {
 	
 	@Override
 	public String reconocer(byte[] imagenCara) {
-		try {
-			HttpResponse<String> result = HttpClient.newBuilder().build().send(
-					HttpRequest.newBuilder().GET().uri(URI.create("http://localhost:8085/reconocedor/matias")).build(), 
-					BodyHandlers.ofString());
-			return result.body();
-		} catch (IOException | InterruptedException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-		return null;
+		RestTemplate restTemplate = new RestTemplate();
+		ResponseEntity<ReconocimientoResultsDto> result = restTemplate.getForEntity("http://localhost:8085/reconocedor/matias", ReconocimientoResultsDto.class);
+		return result.toString();
 	}
 
 }
