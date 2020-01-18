987ed68d2000fcab4def87eb5014bdd55a172b4f
Maxi Astengo <maxiastengo@gmail.com>
2020-01-18 17:00:41 -0300

    Security

--- spring-demo-thea/src/main/java/edu/caece/app/config/security/WebSecurityConfig.java
+++ spring-demo-thea/src/main/java/edu/caece/app/config/security/WebSecurityConfig.java
@@ -59,10 +59,11 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 				.antMatchers("/authenticate").permitAll()
 				.antMatchers("/hello").hasAnyRole("ADMIN")
 				// all other requests need to be authenticated
-				.anyRequest().authenticated().and().
+				.anyRequest().authenticated()
+				.and()
 				// make sure we use stateless session; session won't be used to
 				// store user's state.
-				exceptionHandling().authenticationEntryPoint(jwtAuthenticationEntryPoint).and().sessionManagement()
+				.exceptionHandling().authenticationEntryPoint(jwtAuthenticationEntryPoint).and().sessionManagement()
 				.sessionCreationPolicy(SessionCreationPolicy.STATELESS);
 				// Add a filter to validate the tokens with every request
 		httpSecurity.addFilterBefore(jwtRequestFilter, UsernamePasswordAuthenticationFilter.class);
