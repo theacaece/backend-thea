d2b049877bcab23c5eac2355216f8d266e6fddf7
natig0n <natigon87@hotmail.com>
2020-03-22 15:42:44 -0300

    Logs

--- spring-demo-thea/src/main/java/edu/caece/app/controller/ReconocedorController.java
+++ spring-demo-thea/src/main/java/edu/caece/app/controller/ReconocedorController.java
@@ -35,20 +35,16 @@ public class ReconocedorController {
   @RequestMapping(value = "/reconocer", method = RequestMethod.POST)
   public ResultadoReconocimiento reconocer(@RequestBody byte[] captura) throws Exception {
     log.info(Constantes.INFO_RECONOCIMIENTO);
-    try {
-      if (this.esImagenValida(captura)) {
-        Persona persona = reconocimientoService.reconocerIngreso(captura);
-        if (persona != null) {
-          return new ResultadoReconocimiento(true, persona.isHabilitado(),
-              persona.getNombreCompleto(), Constantes.LOG_BIENVENIDA);
-        }
-        return new ResultadoReconocimiento(false, false, null,
-            Constantes.ERROR_PERSONA_NORECONOCIDA);
+    if (this.esImagenValida(captura)) {
+      Persona persona = reconocimientoService.reconocerIngreso(captura);
+      if (persona != null) {
+        return new ResultadoReconocimiento(true, persona.isHabilitado(),
+            persona.getNombreCompleto(), Constantes.LOG_BIENVENIDA);
       }
-    } catch (Exception e) {
+      return new ResultadoReconocimiento(false, false, null, Constantes.ERROR_PERSONA_NORECONOCIDA);
+    } else {
       throw new Exception(Constantes.ERROR_IMAGEN_INVALIDA);
     }
-    return null;
   }
 
   public void registrarPersona(@RequestParam Long id, @RequestBody byte[] payload) {
